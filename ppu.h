a65aca5b2ba26ab7561085b316c2a720f4fd2111
sORTaC <59201614+sORTaC@users.noreply.github.com>
2020-12-29 00:15:49 +0530

    Update ppu.h

--- ppu.h
+++ ppu.h
@@ -10,11 +10,19 @@ class NesPPU
 {
 private:
 	uint8_t ppu_ram[0x4000];
+	uint8_t primary_oam[64 * 4];
+	uint8_t secondary_oam[64];
 	int ppu_cycles, ppu_scanline;
-	bool horizontal_mirroring = true;
+	bool horizontal_mirroring = false;
 	Bus* busPtr;
 	uint8_t *ptrToBus;
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+	SDL_Texture* texture;
+	Uint32* pixels;
 public:
+	NesPPU();
+	~NesPPU();
 	void ppu_powerup();
 	void ppu_set_vblank();
 	void ppu_clear_vblank();
@@ -22,6 +30,8 @@ public:
 	void ConnectPPU(uint8_t* ptr) { ptrToBus = ptr; }
 	uint16_t ppu_read(int addr);
 	void ppu_write(int addr, uint8_t data);
+	uint16_t ppu_read_oam(int addr) { return primary_oam[addr]; }
+	void ppu_write_oam(int addr, uint8_t data) { primary_oam[addr] = data; }
 	void setScanline(int n) { ppu_scanline = n; }
 	uint16_t interleave(uint16_t, uint16_t);
 	void drawNametable();
@@ -29,6 +39,3 @@ public:
 };
 
 #endif //NESPPU_H
-
-
-
