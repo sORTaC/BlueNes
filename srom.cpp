2c7027d48d957ffaa3ec73b5b160b055703f78dd
sORTaC <59201614+sORTaC@users.noreply.github.com>
2021-02-01 14:23:05 +0530

    Update srom.cpp

--- srom.cpp
+++ srom.cpp
@@ -2,7 +2,10 @@
 
 void srom::mapperLoad(const char* fileAddress)
 {
-	control = 0;
+	reg_control = 0;
+	reg_chr0 = 0;
+	reg_chr1 = 0;
+	reg_prg = 0;
 	prgRomSize = 0;
 	chrRomSize = 0;
 	header.load(fileAddress, 0, 0x10);
@@ -22,7 +25,8 @@ void srom::mapperWrite(int addr, int data)
 		{
 			mregister = 0;
 			counter = 0;
-			control = 0;
+			reg_control |= 0xC;
+			recalculate();
 		}
 		else
 		{
@@ -33,65 +37,71 @@ void srom::mapperWrite(int addr, int data)
 			{
 				if (addr >= 0x8000 && addr <= 0x9fff)
 				{
-					control = mregister & 0x1F;
-					switch (control & 0x3)
-					{
-					case 0:
-					case 1:
-					case 2:
-						mapper = 0;
-						break;
-					case 3:
-						mapper = 1;
-						break;
-					}
+					reg_control = mregister & 0x1f;
 				}
 				else if (addr >= 0xa000 && addr <= 0xbfff)
 				{
-					if (control & 0x10)
-					{
-						chrLO = mregister & 0x1f;
-					}
-					else
-					{
-						CHR = mregister & 0x1e;
-					}
+					reg_chr0 = mregister & 0x1f;
 				}
 				else if (addr >= 0xc000 && addr <= 0xdfff)
 				{
-					if (control & 0x10)
-					{
-						chrHI = mregister & 0x1f;
-					}
+					reg_chr1 = mregister & 0x1f;
 				}
 				else if (addr >= 0xe000 && addr <= 0xffff)
 				{
-					uint8_t mode = (control >> 2) & 0x3;
-					switch (mode)
-					{
-					case 0:
-					case 1:
-					{
-						PRG = (mregister & 0xe) >> 1;
-						break;
-					}
-					case 2:
-					{
-						prgLO = 0;
-						prgHI = mregister & 0xf;
-						break;
-					}
-					case 3:
-					{
-						prgLO = mregister & 0xf;
-						prgHI = prgRomSize - 1;
-					}
-					}
+					reg_prg = mregister & 0x1f;
 				}
+				recalculate();
 				mregister = 0;
 				counter = 0;
 			}
 		}
+	}
+}
+
+void srom::recalculate()
+{
+	//control = mregister & 0x1F;
+
+	switch (reg_control & 0x3)
+	{
+	case 0:
+	case 1:
+	case 2:
+		mapper = 0;
+		break;
+	case 3:
+		mapper = 1;
+		break;
+	}
+
+	if (reg_control & 0x10){chrLO = reg_chr0 & 0x1f;}
+	else { CHR = (reg_chr0 & 0x1e) >> 0; }
+
+	if (reg_control & 0x10){chrHI = reg_chr1 & 0x1f;}
+
+	uint8_t mode = (reg_control >> 2) & 0x3;
+
+	switch (mode)
+	{
+	case 0:
+	case 1:
+	{
+		PRG = (reg_prg & 0xe) >> 1;
+		break;
+	}
+	case 2:
+	{
+		prgLO = 0;
+		prgHI = reg_prg & 0xf;
+		break;
+	}
+	case 3:
+	{
+		prgLO = reg_prg & 0xf;
+		prgHI = prgRomSize - 1;
+		break;
+	}
 	}
 }
 
@@ -100,7 +110,7 @@ uint8_t srom::mapperRead(int addr)
 	int mapperAddr = 0;
 	if (addr >= 0x8000)
 	{
-		if (control & 0x8)
+		if (reg_control & 0x8)
 		{
 			if(addr >= 0x8000 && addr <= 0xbfff)
 				mapperAddr = prgLO * 0x4000 + (addr & 0x3fff);
@@ -120,8 +130,8 @@ uint8_t srom::mapperReadCHR(int addr)
 {
 	if (chrRomSize != 0)
 	{
-		uint8_t mapperAddr = 0;
-		if (control & 0x10)
+		int mapperAddr = 0;
+		if ( reg_control & 0x10)
 		{
 			if (addr >= 0x000 && addr <= 0xfff)
 			{
@@ -137,11 +147,11 @@ uint8_t srom::mapperReadCHR(int addr)
 		{
 			mapperAddr = CHR * 0x2000 + (addr & 0x1fff);
 		}
-		return CartridgeData[0x10 + (prgRomSize - 1) * 0x4000 + mapperAddr];
+		return CartridgeData[0x10 + (prgRomSize) * 0x4000 + mapperAddr];
 	}
 }
 
 uint8_t srom::mapperReadRAM(int addr)
 {
-	return CartridgeData[0x4000 * (prgRomSize - 1) + 0x2000 * (chrRomSize - 1) + addr];
+	return CartridgeData[0x4000 * (prgRomSize) + 0x2000 * (chrRomSize) + addr];
 }
